39013d9280fb9c8bb057bfc6ee1891095a91898c
einliterflasche <einliterflasche@pm.me>
2025-07-02 12:25:31 +0200

    ignore sha

--- monero-sys/vendor/vcpkg-overlays/winflexbison/portfile.cmake
+++ monero-sys/vendor/vcpkg-overlays/winflexbison/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lexxmark/winflexbison
     REF v2.5.25
-    SHA512 18a1a6a0b38b9f44b0f06c0d02aa0a9b6bc65a03ad4c1b6b9e3f1b9e4bc1c3a2b3e4c5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8
+    SHA512 0
     HEAD_REF master
 )
 
