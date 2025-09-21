e4a827a8ddf8e48d46be088535216c615f322f84
binarybaron <binarybaron@mail.mail>
2025-09-21 05:59:26 -0400

    add DEFAULT_DEVELOPER_TIP_ADDRESS_STAGENET

--- swap-asb/src/main.rs
+++ swap-asb/src/main.rs
@@ -287,7 +287,11 @@ pub async fn main() -> Result<()> {
                     (
                         tip,
                         monero::Address::from_str(
-                            swap_env::defaults::DEFAULT_DEVELOPER_TIP_ADDRESS,
+                            match env_config.monero_network {
+                                monero::Network::Mainnet => swap_env::defaults::DEFAULT_DEVELOPER_TIP_ADDRESS_MAINNET,
+                                monero::Network::Stagenet => swap_env::defaults::DEFAULT_DEVELOPER_TIP_ADDRESS_STAGENET,
+                                monero::Network::Testnet => panic!("Testnet is not supported"),
+                            },
                         )
                         .expect("Hardcoded developer tip address to be valid"),
                     )
