f635de4ab8fb807971774c607b2399165e62a9bb
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-11-11 22:02:53 +0100

    feat(swap-feed): add kucoin backend
    $ cargo build -p swap-feed  && target/debug/kucoin_ticker
       Compiling swap-feed v0.1.0 (/srv/another-bout/swap-feed)
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 11.13s
    2025-11-11T21:01:38.040418Z DEBUG hyper_util::client::legacy::connect::http: connecting to 172.64.154.148:443
    2025-11-11T21:01:38.051250Z DEBUG hyper_util::client::legacy::connect::http: connected to 172.64.154.148:443
    2025-11-11T21:01:38.077929Z DEBUG h2::client: binding client connection
    2025-11-11T21:01:38.078433Z DEBUG h2::client: client connection bound
    2025-11-11T21:01:38.078948Z DEBUG h2::codec::framed_write: send frame=Settings { flags: (0x0), enable_push: 0, initial_window_size: 2097152, max_frame_size: 16384, max_header_list_size: 16384 }
    2025-11-11T21:01:38.095145Z DEBUG hyper_util::client::legacy::pool: pooling idle connection for ("https", api.kucoin.com)
    2025-11-11T21:01:38.094988Z DEBUG Connection{peer=Client}: h2::codec::framed_write: send frame=WindowUpdate { stream_id: StreamId(0), size_increment: 5177345 }
    2025-11-11T21:01:38.097703Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=Settings { flags: (0x0), max_concurrent_streams: 100, initial_window_size: 65536, max_frame_size: 16777215 }
    2025-11-11T21:01:38.098912Z DEBUG Connection{peer=Client}: h2::codec::framed_write: send frame=Settings { flags: (0x1: ACK) }
    2025-11-11T21:01:38.099635Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=WindowUpdate { stream_id: StreamId(0), size_increment: 2147418112 }
    2025-11-11T21:01:38.100523Z DEBUG Connection{peer=Client}: h2::codec::framed_write: send frame=Headers { stream_id: StreamId(1), flags: (0x5: END_HEADERS | END_STREAM) }
    2025-11-11T21:01:38.105480Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=Settings { flags: (0x1: ACK) }
    2025-11-11T21:01:38.105970Z DEBUG Connection{peer=Client}: h2::proto::settings: received settings ACK; applying Settings { flags: (0x0), enable_push: 0, initial_window_size: 2097152, max_frame_size: 16384, max_header_list_size: 16384 }
    2025-11-11T21:01:38.430969Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=Headers { stream_id: StreamId(1), flags: (0x4: END_HEADERS) }
    2025-11-11T21:01:38.431418Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=Data { stream_id: StreamId(1) }
    2025-11-11T21:01:38.431629Z DEBUG Connection{peer=Client}: h2::codec::framed_read: received frame=Data { stream_id: StreamId(1), flags: (0x1: END_STREAM) }
    2025-11-11T21:01:38.441155Z DEBUG swap_feed::kucoin::connection: ws_url=wss://ws-api-spot.kucoin.com/?token=2neAiuYvAU61ZDXANAGAsiL4-iAExhsBXZxftpOeh_55i3Ysy2q2LEsEWU64mdzUOPusi34M_wGoSf7iNyEWJ836gMrSSKiHQcT86boTTGyAyHQx3p-pUNiYB9J6i9GjsxUuhPw3Blq6rhZlGykT3Vp1phUafnulOOpts-MEmEH7yGKYwJHVxWnmwU2KdPcHJBvJHl5Vs9Y=.AurJAQJvVLxlY5rxS5oSYg==
    2025-11-11T21:01:39.530990Z DEBUG swap_feed::kucoin::connection: Connected to Kucoin websocket API
    2025-11-11T21:01:39.777267Z DEBUG swap_feed::kucoin::connection: Subscribed to updates for ticker
    Price update: 0.00362000 BTC
    Price update: 0.00362000 BTC
    Price update: 0.00362000 BTC
    Price update: 0.00362100 BTC
    Price update: 0.00362000 BTC
    Price update: 0.00362000 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    Price update: 0.00361900 BTC
    
    Ref: #227

---
+++ swap-feed/src/bin/kucoin_ticker.rs
@@ -0,0 +1,21 @@
+use anyhow::{Context, Result};
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::fmt().with_env_filter("debug").finish(),
+    )?;
+
+    let price_ticker_rest_url_kucoin = Url::parse("https://api.kucoin.com/api/v1/bullet-public")?;
+    let mut ticker =
+        swap_feed::kucoin::connect(price_ticker_rest_url_kucoin, reqwest::Client::new())
+            .context("Failed to connect to kucoin")?;
+
+    loop {
+        match ticker.wait_for_next_update().await? {
+            Ok(update) => println!("Price update: {}", update.ask),
+            Err(e) => println!("Error: {е:#}"),
+        }
+    }
+}
