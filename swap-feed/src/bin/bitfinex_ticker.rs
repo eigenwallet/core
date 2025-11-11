eef7c45b0c654aad657d6e814fe840a2aa9dbcbd
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-11-11 19:39:22 +0100

    feat(swap-feed): add bitfinex backend
    Ref: #227

---
+++ swap-feed/src/bin/bitfinex_ticker.rs
@@ -0,0 +1,20 @@
+use anyhow::{Context, Result};
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::fmt().with_env_filter("debug").finish(),
+    )?;
+
+    let price_ticker_ws_url = Url::parse("wss://api-pub.bitfinex.com/ws/2")?;
+    let mut ticker = swap_feed::bitfinex::connect(price_ticker_ws_url)
+        .context("Failed to connect to bitfinex")?;
+
+    loop {
+        match ticker.wait_for_next_update().await? {
+            Ok(update) => println!("Price update: {}", update.ask),
+            Err(e) => println!("Error: {e:#}"),
+        }
+    }
+}
