03b12c87f7f6ccf20fbf07bd38231386f9f235af
Binarybaron <binarybaron@protonmail.com>
2025-12-01 21:23:31 +0100

    feat(gui): use react compiler to improve performance
    https://react.dev/learn/react-compiler/

--- src-gui/eslint.config.js
+++ src-gui/eslint.config.js
@@ -3,12 +3,14 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import importPlugin from "eslint-plugin-import";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
   { ignores: ["node_modules", "dist"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooksPlugin.configs.flat.recommended,
   {
     languageOptions: {
       globals: globals.browser,
