72ed29335133cc5c96b4fb1a4fabd5e2b7facb85
Binarybaron <binarybaron@protonmail.com>
2025-12-02 21:41:41 +0100

    feat(swap-env): allow ticker url to be left out from config

--- swap-env/src/config.rs
+++ swap-env/src/config.rs
@@ -1,4 +1,7 @@
-use crate::defaults::GetDefaults;
+use crate::defaults::{
+    GetDefaults, BITFINEX_PRICE_TICKER_WS_URL, KRAKEN_PRICE_TICKER_WS_URL,
+    KUCOIN_PRICE_TICKER_REST_URL,
+};
 use crate::env::{Mainnet, Testnet};
 use crate::prompt;
 use anyhow::{bail, Context, Result};
@@ -98,8 +101,11 @@ pub struct Maker {
     #[serde(with = "::bitcoin::amount::serde::as_btc")]
     pub max_buy_btc: bitcoin::Amount,
     pub ask_spread: Decimal,
+    #[serde(default = "default_price_ticker_ws_url_kraken")]
     pub price_ticker_ws_url_kraken: Url,
+    #[serde(default = "default_price_ticker_ws_url_bitfinex")]
     pub price_ticker_ws_url_bitfinex: Url,
+    #[serde(default = "default_price_ticker_rest_url_kucoin")]
     pub price_ticker_rest_url_kucoin: Url,
     #[serde(default, with = "swap_serde::bitcoin::address_serde::option")]
     pub external_bitcoin_redeem_address: Option<bitcoin::Address>,
@@ -112,6 +118,18 @@ pub struct Maker {
 fn default_developer_tip() -> Decimal {
     // By default, we do not tip
     Decimal::ZERO
+}
+
+fn default_price_ticker_ws_url_kraken() -> Url {
+    Url::parse(KRAKEN_PRICE_TICKER_WS_URL).expect("default kraken ws url to be valid")
+}
+
+fn default_price_ticker_ws_url_bitfinex() -> Url {
+    Url::parse(BITFINEX_PRICE_TICKER_WS_URL).expect("default bitfinex ws url to be valid")
+}
+
+fn default_price_ticker_rest_url_kucoin() -> Url {
+    Url::parse(KUCOIN_PRICE_TICKER_REST_URL).expect("default kucoin rest url to be valid")
 }
 
 impl Config {
