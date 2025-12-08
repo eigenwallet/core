d4c5b9012768d9e75ae3f43ab41d30d1baa0a554
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-12-08 17:12:43 +0100

    fix(swap-fs): fix documentation for swap-fs

--- swap-fs/src/lib.rs
+++ swap-fs/src/lib.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 
 /// This is the default location for the overall config-dir specific by system
 // Linux: /home/<user>/.config/xmr-btc-swap/
-// OSX: /Users/<user>/Library/Preferences/xmr-btc-swap/
+// OSX: /Users/<user>/Library/Application Support/xmr-btc-swap/
 pub fn system_config_dir() -> Result<PathBuf> {
     dirs::config_dir()
         .map(|cd| cd.join("xmr-btc-swap"))
@@ -12,7 +12,7 @@ pub fn system_config_dir() -> Result<PathBuf> {
 
 /// This is the default location for the overall data-dir specific by system
 // Linux: /home/<user>/.local/share/xmr-btc-swap/
-// OSX: /Users/<user>/Library/ApplicationSupport/xmr-btc-swap/
+// OSX: /Users/<user>/Library/Application Support/xmr-btc-swap/
 pub fn system_data_dir() -> Result<PathBuf> {
     dirs::data_dir()
         .map(|cd| cd.join("xmr-btc-swap"))
