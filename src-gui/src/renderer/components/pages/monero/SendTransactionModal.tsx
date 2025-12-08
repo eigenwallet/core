c324dc376bcc082b8cabb6f38d6bc5502f272d10
Binarybaron <binarybaron@protonmail.com>
2025-12-08 16:13:03 +0100

    do not close on backdrop click

--- src-gui/src/renderer/components/pages/monero/SendTransactionModal.tsx
+++ src-gui/src/renderer/components/pages/monero/SendTransactionModal.tsx
@@ -27,7 +27,13 @@ export default function SendTransactionModal({
 
   const showSuccess = successResponse !== null;
 
-  const handleClose = () => {
+  const handleClose = (event: unknown, reason: string) => {
+    // We want the user to explicitly close the dialog.
+    // We do not close the dialog upon a backdrop click.
+    if (reason === "backdropClick") {
+      return;
+    }
+
     onClose();
     setSuccessResponse(null);
   };
