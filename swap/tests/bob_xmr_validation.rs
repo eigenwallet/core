1919f16ec014e91ba40016a6d08534d9d22d8ed5
codebestia <codebestia@gmail.com>
2025-12-24 14:33:11 +0100

    test: (integration) alice locks significantly wrong amount

--- swap/tests/bob_xmr_validation.rs
+++ swap/tests/bob_xmr_validation.rs
@@ -64,7 +64,6 @@ async fn given_alice_locks_wrong_xmr_amount_bob_rejects() {
 #[tokio::test]
 async fn given_correct_xmr_amount_bob_redeems_btc() {
     harness::setup_test(FastCancelConfig, None, |mut ctx| async move {
-        // Run both parties through the happy path
         let (bob_swap, bob_join_handle) = ctx.bob_swap().await;
         let bob_swap_id = bob_swap.id;
         
@@ -107,9 +106,58 @@ async fn given_correct_xmr_amount_bob_redeems_btc() {
     .await;
 }
 
+#[tokio::test]
+async fn given_significantly_wrong_xmr_amount_bob_immediately_aborts() {
+    harness::setup_test(SlowCancelConfig, None, |mut ctx| async move {
+        let (bob_swap, bob_join_handle) = ctx.bob_swap().await;
+        let bob_swap_id = bob_swap.id;
+        
+        let bob_swap = tokio::spawn(bob::run_until(bob_swap, |s| {
+            matches!(s, BobState::WaitingForCancelTimelockExpiration { .. })
+        }));
+        
+        let alice_swap = ctx.alice_next_swap().await;
+        let alice_state = alice::run_until(
+            alice_swap,
+            |s| matches!(s, AliceState::BtcLocked { .. }),
+            FixedRate::default(),
+        )
+        .await?;
+        
+        // Alice locks half the expected amount
+        ctx.restart_alice().await;
+        let mut alice_swap = ctx.alice_next_swap().await;
+        alice_swap.state = alice_state.halve_xmr_amount();
+        
+        let alice_swap = tokio::spawn(alice::run(alice_swap, FixedRate::default()));
+        
+        // Bob immediately detects the problem
+        let bob_state = bob_swap.await??;
+        assert!(matches!(bob_state, BobState::WaitingForCancelTimelockExpiration { .. }));
+        
+        let (bob_swap, _) = ctx
+            .stop_and_resume_bob_from_db(bob_join_handle, bob_swap_id)
+            .await;
+        
+        let bob_state = bob::run_until(bob_swap, |s| {
+            matches!(s, BobState::BtcRefunded(..))
+        })
+        .await?;
+        
+        ctx.assert_bob_refunded(bob_state).await;
+        let alice_state = alice_swap.await??;
+        ctx.assert_alice_refunded(alice_state).await;
+        
+        Ok(())
+    })
+    .await;
+}
+
 trait FakeModifyMoneroAmount {
     /// Reduces the Monero amount by one piconero
     fn lower_by_one_piconero(self) -> Self;
+    // Halves the Monero amount
+    fn halve_xmr_amount(self) -> Self;
 }
 
 impl FakeModifyMoneroAmount for AliceState {
@@ -122,6 +170,13 @@ impl FakeModifyMoneroAmount for AliceState {
 
         state3.xmr = state3.xmr.checked_sub(one_piconero).unwrap();
 
+        Self::BtcLocked { state3 }
+    }
+    fn halve_xmr_amount(self) -> Self {
+        let AliceState::BtcLocked { mut state3 } = self else {
+            panic!("Expected BtcLocked state to be able to modify the Monero amount");
+        };
+        state3.xmr = Amount::from_piconero(state3.xmr.as_piconero() / 2);
         Self::BtcLocked { state3 }
     }
 }
