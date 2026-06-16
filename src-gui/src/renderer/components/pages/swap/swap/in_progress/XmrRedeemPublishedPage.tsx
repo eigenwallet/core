70dde19945c7a7448cf3de16e68d80e7a0776cdb
Einliterflasche <81313171+Einliterflasche@users.noreply.github.com>
2026-06-16 20:37:13 +0200

    feat: show manual Monero redeem keys on all pages where it might make sense (#1120)

--- src-gui/src/renderer/components/pages/swap/swap/in_progress/XmrRedeemPublishedPage.tsx
+++ src-gui/src/renderer/components/pages/swap/swap/in_progress/XmrRedeemPublishedPage.tsx
@@ -1,11 +1,14 @@
 import { DialogContentText } from "@mui/material";
 import { TauriSwapProgressEventContent } from "models/tauriModelExt";
+import { SwapMoneroRecoveryButton } from "renderer/components/pages/history/table/SwapMoneroRecoveryButton";
+import { useActiveSwapInfo } from "store/hooks";
 import MoneroTransactionInfoBox from "../components/MoneroTransactionInfoBox";
 
 export default function XmrRedeemPublishedPage(
   state: TauriSwapProgressEventContent<"XmrRedeemPublished">,
 ) {
   const xmr_redeem_txid = state.xmr_redeem_txids[0] ?? null;
+  const swap = useActiveSwapInfo();
 
   return (
     <>
@@ -19,6 +22,16 @@ export default function XmrRedeemPublishedPage(
         additionalContent={null}
         loading
       />
+      {swap && (
+        <SwapMoneroRecoveryButton
+          swap={swap}
+          variant="text"
+          size="small"
+          sx={(theme) => ({ color: theme.palette.text.secondary })}
+        >
+          Redeem manually
+        </SwapMoneroRecoveryButton>
+      )}
     </>
   );
 }
