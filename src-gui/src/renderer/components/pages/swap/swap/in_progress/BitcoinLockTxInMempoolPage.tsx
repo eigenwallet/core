1c50b25ee56eab721677beebfd78ae2d3b9f9e7e
Mohan <86064887+binarybaron@users.noreply.github.com>
2026-06-25 11:19:52 +0200

    fix(gui): always show lock-tx description on BitcoinLockTxInMempoolPage (#1141)
    Co-authored-by: binarybaron <user@mbp-von-user.speedport.ip>

--- src-gui/src/renderer/components/pages/swap/swap/in_progress/BitcoinLockTxInMempoolPage.tsx
+++ src-gui/src/renderer/components/pages/swap/swap/in_progress/BitcoinLockTxInMempoolPage.tsx
@@ -3,9 +3,6 @@ import { formatConfirmations } from "utils/formatUtils";
 import BitcoinTransactionInfoBox from "renderer/components/pages/swap/swap/components/BitcoinTransactionInfoBox";
 import { Box, DialogContentText } from "@mui/material";
 
-// This is the number of blocks after which we consider the swap to be at risk of being unsuccessful
-const BITCOIN_CONFIRMATIONS_WARNING_THRESHOLD = 2;
-
 export default function BitcoinLockTxInMempoolPage({
   btc_lock_confirmations,
   btc_lock_txid,
@@ -20,10 +17,7 @@ export default function BitcoinLockTxInMempoolPage({
 
   return (
     <>
-      {(btc_lock_confirmations === undefined ||
-        btc_lock_confirmations < BITCOIN_CONFIRMATIONS_WARNING_THRESHOLD) && (
-        <DialogContentText>{description()}</DialogContentText>
-      )}
+      <DialogContentText>{description()}</DialogContentText>
       <Box
         style={{
           display: "flex",
@@ -35,15 +29,7 @@ export default function BitcoinLockTxInMempoolPage({
           title="Bitcoin Lock Transaction"
           txId={btc_lock_txid}
           loading
-          additionalContent={
-            <>
-              Most makers require one confirmation before locking their Monero.
-              After they lock their funds and the Monero transaction receives
-              one confirmation, the swap will proceed to the next step.
-              <br />
-              Confirmations: {formatConfirmations(btc_lock_confirmations)}
-            </>
-          }
+          additionalContent={<>{formatConfirmations(btc_lock_confirmations)}</>}
         />
       </Box>
     </>
