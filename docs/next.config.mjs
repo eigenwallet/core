024bf8667fb686fb0ac0fc7912f9babe2a935a19
einliterflasche <einliterflasche@pm.me>
2026-07-13 14:56:53 +0200

    fix docs build

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,5 +3,8 @@ import nextra from "nextra";
 const withNextra = nextra({});
 
 export default withNextra({
-  output: "standalone",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 });
